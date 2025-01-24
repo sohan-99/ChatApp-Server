95b3946a1b248a68cf4b329286a026122935a392
sohan_99 <sohan75632@gmail.com>
2025-01-25 02:20:51 +0600

    Backend JWT Token complete

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,3 +1,4 @@
+import createTokenAndSaveCookie from "../jwt/generateToken.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
@@ -26,11 +27,47 @@ export const signup = async (req, res) => {
     });
 
     await newUser.save();
-    res.status(201).json({ message: "User registered successfully" });
-    // console.log(req.body);  // Debug request body
+    if (newUser) {
+      createTokenAndSaveCookie(newUser._id, res);
+      res
+        .status(201)
+        .json({ message: "User registered successfully", newUser });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!user || !isMatch) {
+      return res.status(404).json({ message: "invalid User or Password" });
+    }
+    createTokenAndSaveCookie(user._id, res);
+    res.status(201).json({
+      message: "User logged in successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
 
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server error" });
   }
 };
+export const logout = async(req,res)=>{
+  try {
+    res.clearCookie('jwt');
+    res.status(200).json({ message: "User logged out successfully" });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
